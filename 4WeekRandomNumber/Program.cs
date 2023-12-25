73bce6ae95c1e4affe1d04fa5262008acfb78c16
Irina Belik. <ibelik@tktk.ee>
2023-12-25 16:31:49 +0200

    add 4week code

---
+++ 4WeekRandomNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//int sum = 0;
+
+//for (int i = 1; i < 4; i++) /*i--=i-1*/
+//{
+//    Console.WriteLine($"i={i}");
+//    Console.WriteLine($"Current total: {sum}");
+//    sum = sum + 1;
+//}
+
+Random rnd = new Random();
+
+int myRandomNum;
+    int randomSum = 0;
+
+for (int i = 0; i < 3; i++)
+{
+myRandomNum = rnd.Next(0, 11); /*0-10*/
+    randomSum = randomSum + myRandomNum;
+Console.WriteLine($"My random number is: {myRandomNum}");
+}
+Console.WriteLine($"Random sum total: {randomSum}");
---
+++ Week4Loop/Program.cs
@@ -0,0 +1,14 @@
+﻿// FOR - LOOP
+//dry-principle - Do Not Repeat Yourself 
+
+for (int i = 0; i < 5; i++) /*i++ = i+1*/
+{
+    Console.WriteLine("We don't need no education.");
+}
+Console.WriteLine("Have a nice day.");
+
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
